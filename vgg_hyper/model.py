671a4f7289983a8b3ba3c61476629fb92c01e8cb
Todd <myoung44@vols.utk.edu>
2017-12-20 11:16:10 -0500

    Cleaning up

--- vgg_hyper/model.py
+++ vgg_hyper/model.py
@@ -1,56 +1,60 @@
-import keras
 from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Dropout
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
-import numpy as np
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
 
-def VGG_16(kernel1=3, kernel2=3, kernel3=3, kernel4=3, kernel5=3, kernel6=3,
-           kernel7=3, kernel8=3, kernel9=3, kernel10=3, dropout1=0.5, dropout2=0.5):
+
+def vgg16(kernel1=3, kernel2=3, kernel3=3, kernel4=3, kernel5=3, kernel6=3,
+             kernel7=3, kernel8=3, kernel9=3, kernel10=3, dropout1=0.25,
+             dropout2=0.25, dropout3=0.25, dropout4=0.25, dropout5=0.25, dropout6=0.25):
     """
-    Defines the VGG16 model.
+    VGG16.
+
+    Parameters
+    ----------
+    `kernel*` [int, default=3]
+        Convolution kernel size.
+
+    `dropout*` [float, default=0.25]
+        Dropout proportion at the end of each block.
+
+    Returns
+    -------
+    `model` [keras.models.Sequential()]
     """
     model = Sequential()
-    model.add(ZeroPadding2D((1,1), input_shape=(32, 32, 3)))
-    model.add(Convolution2D(64, kernel_size=(kernel1, kernel1), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(64, kernel_size=(kernel2, kernel2), activation='relu'))
-    model.add(MaxPooling2D((1,1), strides=(2,2)))
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(128, kernel_size=(kernel3, kernel3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(128, kernel_size=(kernel4, kernel4), activation='relu'))
-    model.add(MaxPooling2D((1,1), strides=(2,2)))
+    # Block 1
+    model.add(Conv2D(32, kernel_size=(kernel1, kernel1), padding='same', activation='relu', input_shape=(32, 32, 3)))
+    model.add(Conv2D(64, kernel_size=(kernel2, kernel2), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(dropout1))
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, kernel_size=(kernel5, kernel5), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, kernel_size=(kernel6, kernel6), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, kernel_size=(kernel7, kernel7), activation='relu'))
-    model.add(MaxPooling2D((1,1), strides=(2,2)))
+    # Block 2
+    model.add(Conv2D(128, kernel_size=(kernel3, kernel3), padding='same', activation='relu'))
+    model.add(Conv2D(128, kernel_size=(kernel4, kernel4), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(dropout2))
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, kernel_size=(kernel8, kernel8), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, kernel_size=(kernel9, kernel9), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((1,1), strides=(2,2)))
+    # Block 3
+    model.add(Conv2D(256, kernel_size=(kernel5, kernel5), padding='same', activation='relu'))
+    model.add(Conv2D(256, kernel_size=(kernel6, kernel6), padding='same', activation='relu'))
+    model.add(Conv2D(256, kernel_size=(kernel7, kernel7), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(dropout3))
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, kernel_size=(kernel10, kernel10), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((1,1), strides=(2,2)))
+    # Block 4
+    model.add(Conv2D(512, kernel_size=(kernel8, kernel8), padding='same', activation='relu'))
+    model.add(Conv2D(512, kernel_size=(kernel9, kernel9), padding='same', activation='relu'))
+    model.add(Conv2D(512, kernel_size=(kernel10, kernel10), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(dropout4))
 
     model.add(Flatten())
     model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(dropout1))
+    model.add(Dropout(dropout5))
     model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(dropout2))
-    model.add(Dense(5000, activation='softmax'))
+    model.add(Dropout(dropout6))
+    model.add(Dense(10, activation='softmax'))
 
     return model
